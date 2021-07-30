68112dcb3491344628f4713f2ecca9d4506be64e
oussema-yahyaoui <ousema.benabdallah.yahyaoui@gmail.com>
2021-07-30 03:42:17 +0100

    test

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,23 @@
-const express =require("express")
+const express = require("express")
 const app = express();
-const mongoose =require("mongoose")
-const dotenv =require("dotenv")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true,
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 }
-).then(()=>console.log("cannected with DB  succsessfully "))
-.catch(err=>console.log(err));
+).then(() => console.log("cannected with DB  succsessfully "))
+    .catch(err => console.log(err));
 
 app.use(express.json())
-app.use("api/auth",authRoute)
+app.use("backend/auth", authRoute)
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("backend server is running in port 3000")
 })
